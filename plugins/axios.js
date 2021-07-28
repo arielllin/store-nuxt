5b040f2161b625e2ad28473435d88b1ef436d405
Sam.Lin <Sam.Lin@criltds.com>
2021-07-29 01:20:18 +0800

    modify axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,23 +1,8 @@
 import { setClient } from '@/api/apiClient'
 
 export default function({ $axios }, inject) {
-  // Create a custom axios instance
-  const api = $axios.create({
-    headers: {
-      common: {
-        Accept: 'text/plain, */*'
-      }
-    }
-  })
-
-  // Set baseURL to something different
-  // api.setBaseURL('https://my_api.com')
-
-  // Inject to context as $api
-  // inject('api', api)
-
   // request interceptor
-  api.interceptors.request.use(
+  $axios.interceptors.request.use(
     config => {
       return config
     },
@@ -28,7 +13,7 @@ export default function({ $axios }, inject) {
   )
 
   // response interceptor
-  api.interceptors.response.use(
+  $axios.interceptors.response.use(
     response => {
       return response.data
     },
@@ -38,5 +23,5 @@ export default function({ $axios }, inject) {
     }
   )
 
-  setClient(api)
+  setClient($axios)
 }
